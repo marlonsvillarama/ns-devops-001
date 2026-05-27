4a03300d42a3bb4262cf1b8b66111cefa79420e4
Marlon Villarama <marlonsvillarama@gmail.com>
2026-05-27 22:51:47 +1200

    20260527

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,2 @@
-<script>
-    import '$lib/style/app.css';
-    import Button from "$lib/components/ui/Button.svelte";
-    import Input from '$lib/components/ui/Input.svelte';
-    import Select from '$lib/components/ui/Select.svelte';
-    import SelectItem from '$lib/components/ui/SelectItem.svelte';
-</script>
-
-<main>
-    <Button>Create new field</Button>
-    <Input layout="row" placeholder="test" />
-    <Select label="Select an option...">
-        <SelectItem>Option 1</SelectItem>
-        <SelectItem>Option 2</SelectItem>
-        <SelectItem>Option 3</SelectItem>
-    </Select>
-</main>
-
-<style>
-    main {
-        padding: 1rem;
-    }
-</style>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
